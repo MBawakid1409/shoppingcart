a4bec3ba926bc03d627a5826fecce14bf6254553
Mohammad Bawakid <62749199+MBawakid1409@users.noreply.github.com>
2021-05-06 20:31:11 +0300

    Instantiate new class in our shoppingcart application

---
+++ src/shoppingcart/Item.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shoppingcart;
+
+/**
+ *
+ * @author Yank086L
+ */
+public class Item {
+    public int itemID;
+    public String descItem;
+    public double price;
+    public int quantity;
+}
